1c24aa0f19b904975810ab71f0bc56d291851cd7
Vasu <you@example.com>
2023-02-20 12:27:15 +0530

    fix:removed temporary address model

--- model/seller.js
+++ model/seller.js
@@ -1,52 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail, isStrongPassword } = require('validator');
 
-//temporary address schema
-const addressSchema = new mongoose.Schema({
-  UserId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-    required: [true, 'must provide UserId']
-  },
-  addressType: {
-    type: String,
-    required: [true, 'must provide addressType'],
-    enum: ['home', 'work']
-  },
-  name: {
-    type: String,
-    required: [true, 'must provide name'],
-    trim: true,
-    maxlength: [20, 'not more than 20 characters']
-  },
-  contactNumber: {
-    type: String,
-    required: [true, 'must provide contact-number'],
-    unique: [true, 'this contact already exists'],
-    maxlength: [10, 'must provide 10 digit number']
-  },
-  pincode: {
-    type: Number,
-    required: [true, 'must provide pincode'],
-    maxlength: [6, 'pincode consists of 6 digits']
-  },
-  street: {
-    type: String,
-    required: false
-  },
-  locality: {
-    type: String,
-    required: false
-  },
-  city: {
-    type: String,
-    required: [true, 'must provide city']
-  },
-  state: {
-    type: String,
-    required: [true, 'must provide state']
-  }
-});
 const sellerModel = new mongoose.Schema({
   firstName: {
     type: String,
@@ -79,9 +33,10 @@ const sellerModel = new mongoose.Schema({
     type: String,
     required: [true, 'must provide contact-number'],
     unique: true,
+    minlength: 10,
     maxlength: [10, 'contact number should not contain more than 10 characters']
   },
   address: addressSchema
 });
-mongoose.model('Address', addressSchema);
+
 module.exports = mongoose.model('seller', sellerModel);
