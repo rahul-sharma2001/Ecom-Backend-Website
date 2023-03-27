92f7a9e22748fe3ff4562d843b22fa740e2dbd9b
richa tripathi <richa@cloudmatetechnologies.com>
2023-03-27 16:22:24 +0530

    feat:review date

--- model/review.js
+++ model/review.js
@@ -14,6 +14,15 @@ const reviewSchema=new mongoose.Schema({
         required:[true,"must provide rating"],
         trim:true,
     },
+    customerName:{
+        type:String,
+        required:[true,"must provide username"],
+        trim:true,
+    },
+    date:{
+        type:Date,
+        default: Date.now()
+    }
 })
 
 module.exports=mongoose.model('review',reviewSchema)
