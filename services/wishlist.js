f6cadb4be8068eb9ce6fd1feb383329155c20701
richa tripathi <richa@cloudmatetechnologies.com>
2023-04-07 14:11:58 +0530

    feat:images addition

--- services/wishlist.js
+++ services/wishlist.js
@@ -20,6 +20,9 @@ class WishlistService {
         throw new Error('userId is required');
       }
       const getWishlist = await WishlistModel.findOne({ userId: id });
+      if (getWishlist === null) {
+        return 'no data with this id';
+      }
       return getWishlist;
     } catch (error) {
       throw error;
